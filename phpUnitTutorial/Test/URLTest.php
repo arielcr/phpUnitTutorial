8efa3c60229fdc199607c1b9f7ade4defb28bd96
Ariel Orozco <aorozco@77digital.com>
2013-11-08 15:23:15 -0600

    Modified Test Case

--- phpUnitTutorial/Test/URLTest.php
+++ phpUnitTutorial/Test/URLTest.php
@@ -8,6 +8,7 @@ class URLTest extends \PHPUnit_Framework_TestCase
 {
     public function testSluggifyReturnsSluggifiedString()
     {
+		// Testing URL
         $originalString = 'This string will be sluggified';
         $expectedResult = 'this-string-will-be-sluggified';
 
