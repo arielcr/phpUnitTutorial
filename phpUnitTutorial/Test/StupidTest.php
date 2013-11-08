8b27f88bdacb05a80ff84c7ac6b156dd3d5acd11
Ariel Orozco <aorozco@77digital.com>
2013-11-08 10:24:11 -0600

    Testing

--- phpUnitTutorial/Test/StupidTest.php
+++ phpUnitTutorial/Test/StupidTest.php
@@ -6,6 +6,7 @@ class StupidTest extends \PHPUnit_Framework_TestCase
 {
     public function testTrueIsTrue()
     {
+        // test to pass
         $foo = true;
         $this->assertTrue($foo);
     }
